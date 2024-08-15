9750f671f4ab6112c9c5f957c4a3a76f23dec066
Nina-Tekkey <71224084+Nina-Tekkey@users.noreply.github.com>
2024-08-15 17:04:07 -0500

    Update run_PartitionReadsRNA.sh

--- PhasingReads/RNA/run_PartitionReadsRNA.sh
+++ PhasingReads/RNA/run_PartitionReadsRNA.sh
@@ -10,7 +10,7 @@ echo "Loading software..."
 eval $( spack load --sh  python@3)
 eval $( spack load --sh py-pysam@0.15.3/fhxd47q)
 
-read FastQR1 FastQR2 CombinedBAM MatBAM PatBAM mat1 mat2 pat1 pat2 < <( sed -n ${SLURM_ARRAY_TASK_ID}p $1 )
+read FastQR1 FastQR2 CombinedBAM MatBAM PatBAM mat1 pat1 < <( sed -n ${SLURM_ARRAY_TASK_ID}p $1 )
 
 
 #Unzip gzipped FastQs
@@ -23,5 +23,5 @@ echo "unzipped..."
 
 
 echo "Start python script..."
-python3 /scratch/twlab/tekkey/PhasingData/scripts/partitionReadsRNA.py $FastQR1_without_extension $FastQR2_without_extension $CombinedBAM $MatBAM $PatBAM $mat1 $mat2 $pat1 $pat2
+python3 /scratch/twlab/tekkey/PhasingData/scripts/partitionReadsRNA.py $FastQR1_without_extension $FastQR2_without_extension $CombinedBAM $MatBAM $PatBAM $mat1 $pat1
 echo "Python script complete!"
